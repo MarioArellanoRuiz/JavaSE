02e5d37aab2161e6d795a5ef2006a3c446cf8409
mario <mario@mario-Inspiron-5555>
2017-10-16 14:24:36 -0500

    Git + NetBeans

--- PracticasJava/src/Semana4/Suma.java
+++ PracticasJava/src/Semana4/Suma.java
@@ -17,7 +17,11 @@ public class Suma {
         
         System.out.println(resultado);
         
+        double x1 = ((-b)+(Math.sqrt(Math.pow(b, 2)-4*a*c)))/2*a;
+        double x2 = ((-b)-(Math.sqrt(Math.pow(b, 2)-4*a*c)))/2*a;
         
+        System.out.println(x1);
+        System.out.println(x2);
        
     }
 }
