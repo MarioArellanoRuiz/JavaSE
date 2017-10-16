da0e07fb3e26f17a9fc033f9d67d031e61c4f0ac
mario <mario@mario-Inspiron-5555>
2017-10-16 11:47:36 -0500

    Git + NetBeans

---
+++ PracticasJava/src/Semana4/Suma.java
@@ -0,0 +1,23 @@
+
+package Semana4;
+
+
+public class Suma {
+    public static void main(String[] args) {
+        
+        int a=5;
+        int b=5;
+        int suma;
+        suma = a+b;
+        System.out.println(suma);
+        
+        float pi = (float) Math.PI;
+        float c=10.589f;
+        float resultado = pi*c;
+        
+        System.out.println(resultado);
+        
+        
+       
+    }
+}
